11384792af2b58a20544e205ebcca6f6d6b84aac
VargasGui <guilherme.vargas1h3@gmail.com>
2023-10-03 12:33:54 -0300

    React-Aula-0310-Finalizada

--- reactBasic/basic-app/src/components/AppViewer.jsx
+++ reactBasic/basic-app/src/components/AppViewer.jsx
@@ -1,8 +1,17 @@
 import styles from './AppViewer.module.css'
 
+function somarNumeros(a, b) {
+  return a + b
+}
+
+
 const AppViewer = () => {
   return (
-    <div className={styles.avContainer}>AppViewer</div>
+    <>
+      <div className={styles.avContainer}>AppViewer</div>
+      <p>{somarNumeros(5, 17)}</p>
+    </>
+
   )
 }
 
